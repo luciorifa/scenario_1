a80fe157951a2c360ac505d8de95e2abd2533ddd
luciorifa <51023882+luciorifa@users.noreply.github.com>
2021-09-20 17:00:45 -0500

    Update startup.sh

--- modules/GCE/startup.sh
+++ modules/GCE/startup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo apt update 
-sudo apt -y install curl
-sudo apt-get install -y python3-pip
-sudo echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
+
 sudo touch /tmp/pubsub.sh
 
 sudo cat << 'EOF' > /tmp/pubsub.sh
